b3345f939a38a6129760aa4f63ed0e49edba5032
Sue Uyetake <sue.uyetake@gmail.com>
2015-12-30 01:33:21 -0700

    UI signup automation test setup for iteration #6

--- selenium_webdriver_recurly_UI.rb
+++ selenium_webdriver_recurly_UI.rb
@@ -1,53 +1,62 @@
 #!/usr/bin/env ruby
 require 'selenium-webdriver'
 
-# This standalone program will navigate to ReCurly's
+# This standalone program will navigate to Recurly's
 # signup page and make some entries
 
-# unless a gemfile is used and bundled,
+# unless a Gemfile is used and bundled,
 # first install the following gems at the commandline:
 # gem install rubygems
 # gem install selenium-webdriver
 # gem install chromedriver-helper
 
-driver = Selenium::WebDriver.for :chrome
-driver.navigate.to "https://app.recurly.com/signup"
-
 # the element can be found by name or by id
 # <input class="Field-input" id="signup_company" name="signup[company]" size="30" type="text" value=" ">
 # element = driver.find_element(:name, 'signup[company]')
-element = driver.find_element(:id, 'signup_company')
+# element = driver.find_element(:id, 'signup_company')
 
 # Future Mod Fix: asking user for a letter to use results in a stale entry error
 # puts 'Please enter a letter to use in the submission'
 # letter_entry = gets.chomp
 # puts 'Thank you.'
 
-letter_entry = 's'
-
-# since blank entry asks only for a single char, do so, in the spirit of TDD:
-element.send_keys letter_entry
-
-# Test One: populate a letter entry into all the field ids:
+driver = Selenium::WebDriver.for :chrome
+driver.navigate.to "https://app.recurly.com/signup"
 field = ['signup_company', 'signup_first_name', 'signup_last_name',
   'signup_billing_contact_phone', 'signup_password', 'signup_email',
   'signup_subdomain', 'signup_industry', 'signup_estimated_annual_revenue']
+
+## Test One: populate nothing to all field ids and simply submit:
+# element = driver.find_element(:id, 'signup_company')
+# element.submit
+
+## Next Tests:
+## set the automation iteration values here
+# since blank entry asks only for a single char, do so, in the spirit of TDD
+letter_entry = 's'
+numeric = '6'
+password_vanity_url = letter_entry*8 + numeric
+
+
+# Test Two: populate the letter entry into all the field ids:
 field.each do |x|
   element = driver.find_element(:id, x)
   element.send_keys letter_entry
 end
+# element.submit
 
-# Test Two: leave these commented out until Test One results are viewed
+
+# Test Three:
 # The selector fields need particular entries,
 # yet can also be selected with single letter entries:
 element = driver.find_element(:id, 'signup_password')
-element.send_keys 'sssssss3'
+element.send_keys password_vanity_url
 
 element = driver.find_element(:id, 'signup_subdomain')
-element.send_keys 'sssssss3'
+element.send_keys password_vanity_url
 
 element = driver.find_element(:id, 'signup_email')
-element.send_keys 'sue.uyetake3@yahoo.com'
+element.send_keys 'sue.uyetake' + numeric + '@yahoo.com'
 
 element = driver.find_element(:id, 'signup_industry')
 element.send_keys 's'
@@ -64,8 +73,8 @@ puts ''
 puts "Tested Page Title: #{driver.title}"
 puts 'URL: https://app.recurly.com/signup'
 puts ''
-puts 'sue.uyetake3.yahoo.com'
-puts 'ssssssss3.recurly.com'
+puts 'sue.uyetake' + numeric + '@yahoo.com'
+puts 'https://' + password_vanity_url + '.recurly.com'
 puts
 result = driver.find_element(:id, 'content')
 puts result.text
@@ -73,27 +82,3 @@ puts result.text
 # comment out the quit so you can see the browser after test run
 # ..yet you must manually quit the browser instance later
 # driver.quit
-
-
-
-
-
-# puts ''
-# puts "Page Title: #{driver.title}"
-# puts "Element of text box used was: #{element.inspect}"
-# puts ''
-
-# seek the results and within the element containing lat long
-# results = driver.find_element(:id, 'b_results')
-# lat_long = results.find_element(:class, 'b_focusTextMedium')
-
-# puts results.inspect
-# puts "Here is everything on the return page: "
-# puts results.text
-
-# puts lat_long.inspect
-# puts ''
-# puts 'Here is what you really want to compare:'
-# puts lat_long.text
-
-
